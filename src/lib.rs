164773473ef6e7583045a074c3fe5d13c73d9220
kklop <kevin.klop@kevinklop.ca>
2023-11-20 15:45:46 -0800

    started adding AIS sentences

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,23 @@ pub mod heading;
 pub mod miscellaneous;
 pub mod wind;
 pub mod base;
+pub mod ais;
 
-use gps::{gga::Gga, gll::Gll, rmc::Rmc};
-use miscellaneous::{zda::Zda, rsa::Rsa, mtw::Mtw};
+use gps::{gga::Gga, gll::Gll, rmc::Rmc, gsv::Gsv};
+use miscellaneous::{zda::Zda, rsa::Rsa, mtw::Mtw, mda::Mda, xdr::Xdr};
 use heading::{rot::Rot, hdg::Hdg, hdm::Hdm, hdt::Hdt, vtg::Vtg};
 use wind::{mwd::Mwd, mwv::Mwv, vhw::Vhw};
+use ais::Ais;
 
 pub enum Nmea0183 {
+    AIS(Ais),
     GGA(Gga),
     GLL(Gll),
+    GSV(Gsv),
     HDG(Hdg),
     HDM(Hdm),
     HDT(Hdt),
+    MDA(Mda),
     MTW(Mtw),
     MWD(Mwd),
     MWV(Mwv),
@@ -23,5 +28,6 @@ pub enum Nmea0183 {
     RSA(Rsa),
     VHW(Vhw),
     VTG(Vtg),
+    XDE(Xdr),
     ZDA(Zda),
 }
