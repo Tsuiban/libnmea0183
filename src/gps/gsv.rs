95b1702001d7f5fb45330dd7e0262f3dfb3c1c54
kklop <kevin.klop@kevinklop.ca>
2023-11-20 15:31:38 -0800

    added gsv

---
+++ src/gps/gsv.rs
@@ -0,0 +1,43 @@
+use crate::base::*;
+
+pub struct SatelliteData {
+    pub id : usize,
+    pub elevation : usize,
+    pub azimuth : usize,
+    pub snr : usize,
+}
+
+pub struct Gsv {
+    base : Nmea0183Base,
+}
+
+impl Gsv {
+    pub fn new(base: Nmea0183Base) -> Gsv {
+        Gsv { base }
+    }
+
+    pub fn sentence_total(&self) -> UsizeError {
+        self.base.parameter(0)
+    }
+    
+    pub fn sentence_number(&self) -> UsizeError {
+        self.base.parameter(1)
+    }
+    
+    pub fn number_of_satellites(&self) -> UsizeError {
+        self.base.parameter(2)
+    }
+    
+    pub fn satellite(&self, n : usize) -> Result<SatelliteData, NmeaError> {
+        if n*4 + 6 < self.base.parameters.len() {
+            Ok(SatelliteData {
+                id : self.base.parameter(n * 4 + 3)?,
+                elevation : self.base.parameter(n * 4 + 1 + 3)?,
+                azimuth : self.base.parameter(n * 4 + 2 + 3)?,
+                snr : self.base.parameter(n * 4 + 3 + 3)?,
+            })
+        } else {
+            Err(NmeaError("Invalid index".to_string()))
+        }
+    }
+}
